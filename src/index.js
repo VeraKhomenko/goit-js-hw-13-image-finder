c2d84c7f73e091a6709abd3ca5c06b9055d2ba07
VeraKhomenko <pochtavkh@gmail.com>
2021-02-23 14:39:14 +0200

    getting started
     working files created

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './scss/styles.scss';
+
+
+// import fetchCountries from './js/fetchCountries'
+// import addHandlers from './js/addHandlers';
+// import debounce from '../node_modules/lodash.debounce';
+
+// const nameCountry = document.querySelector('.js-name-country');
+
+// function onChangeNameCountry(event) {
+//     if (!event.target.value) {
+//         return;
+//     }
+//     fetchCountries(event.target.value).then(addHandlers).catch(console.error);
+// }
+
+// nameCountry.addEventListener('input', debounce(onChangeNameCountry, 500));
